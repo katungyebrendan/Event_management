6a114d15fd765fe23989dcfb39795589dfffe6a3
katungyebrendan <145586135+katungyebrendan@users.noreply.github.com>
2024-07-09 16:22:22 +0300

    Revert "Rose1"

--- android/app/src/main/kotlin/com/example/helloworld/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,5 @@ package com.example.helloworld
 
 import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
-
-
-
