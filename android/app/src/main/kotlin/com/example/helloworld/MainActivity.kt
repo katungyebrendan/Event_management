5ee60d76e63cd0b864b64ebd1ddb82ff3fd927be
rosemary_akoth <akothrosemary51@gmail.com>
2024-07-09 07:36:30 +0000

    updated

--- android/app/src/main/kotlin/com/example/helloworld/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/helloworld/MainActivity.kt
@@ -2,5 +2,10 @@ package com.example.helloworld
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
 }
+
+
+
