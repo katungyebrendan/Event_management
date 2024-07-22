8d4ba9837ccb7b29cebdbb3d8ce562fce629884a
Kats_Brendan <katsbrendan@gmail.com>
2024-07-22 20:45:46 +0000

    keep user logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
-import 'homepage.dart'; // Import your existing homepage
+import 'homepage.dart';
+import 'package:helloworld/auth/login_page.dart';
+import 'package:helloworld/user/user_home_page.dart';
+import 'package:helloworld/organizer/OrganizerHomePage.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
@@ -18,8 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Disable the debug banner
       title: 'Event Booking App',
-      home:
-          HomePageWrapper(), // Use the HomePageWrapper to initialize NotificationService
+      home: HomePageWrapper(),
     );
   }
 }
@@ -45,8 +49,42 @@ class _HomePageWrapperState extends State<HomePageWrapper> {
 
   @override
   Widget build(BuildContext context) {
-    // Initialize NotificationService if required
+    return AuthWrapper();
+  }
+}
 
-    return HomePage(); // Return the actual HomePage widget
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance
+                .collection('users')
+                .doc(snapshot.data!.uid)
+                .get(),
+            builder: (context, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (userSnapshot.hasData) {
+                String userType = userSnapshot.data!['userType'];
+                if (userType == 'Client') {
+                  return UserHomePage();
+                } else if (userType == 'Event Organizer') {
+                  return OrganizerHomePage();
+                }
+              }
+              return LoginPage();
+            },
+          );
+        } else {
+          return LoginPage();
+        }
+      },
+    );
   }
 }
